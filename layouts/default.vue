49775cd82c8daaffd0f4b77ab59d9bbb86ba3295
Maxim Therrien <maxim@acerspyro.ca>
2021-05-19 00:46:28 -0400

    Initial commit - Placeholder website

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <section class="main-content columns">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;400;500&display=swap');
+
+  #__layout,
+  #__layout > div,
+  #__layout > div > section,
+  #__layout > div > section > div {
+    min-height: 100vh;
+  }
+
+  //:root {
+    //background: #222A2C;
+  //}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hero">
+    <div class="hero-body has-text-centered">
+      <h1 class="title">Imprimis.</h1>
+      <h2 class="subtitle">Coming to a dimension near you.<br/>Soon...</h2>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "~bulma/sass/utilities/_all";
+
+  h1.title {
+    font-size: 3rem;
+    text-shadow:
+      6px 2px 0px transparentize(#000, 0.7),
+      -6px -2px 0px transparentize(#000, 0.5);
+  }
+
+  h2.subtitle {
+    text-shadow:
+      4px 1px 0px transparentize($primary, 0.7),
+      -4px -1px 0px transparentize(adjust-hue($primary, 180deg), 0.5);
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+
+  section.hero {
+    min-height: 100%;
+    display: flex;
+    justify-content: center;
+
+    .hero-body {
+      flex-grow: 0;
+    }
+  }
+
+</style>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
