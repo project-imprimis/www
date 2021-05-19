d2b79f1de36fe6396a0bd23972a378a14b8074e4
Maxim Therrien <maxim@acerspyro.ca>
2021-05-19 01:02:02 -0400

    Fix Google Fonts not loading in build

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,7 +30,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;400;500&display=swap');
 
   #__layout,
   #__layout > div,
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;400;500&display=swap'
+      }
     ]
   },
 
