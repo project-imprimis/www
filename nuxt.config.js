07f9da167894a841aa8c81686e471bbb31423f5d
Luis David Licea Torres <luislicea1011@hotmail.com>
2022-05-07 20:29:46 -0600

    Fix favicon not loading.
    The favicon was not being loaded by gh-pages despite working with `yarn
    dev`. The solution was looking for the favicon in the current directory.
    This meant changing the path from `/favicon.ico` to `./favicon.ico`.

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
       { hid: 'description', name: 'description', content: 'Coming to a dimension near you... Soon.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
       {
         rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;400;500&display=swap'
       }
